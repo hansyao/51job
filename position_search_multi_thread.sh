27cb07a74c1c34e69301968a6767e3869a8f48f6
hansyao <hansyow@gmail.com>
2022-01-14 16:39:18 +0800

    Update position_search_multi_thread.sh

--- position_search_multi_thread.sh
+++ position_search_multi_thread.sh
@@ -163,7 +163,7 @@ function position_search() {
 		fi
 
 		# 转码并格式化
-		iconv -f GB18030 -t UTF-8 <"${TMP_RESPONSE}" | jq -r '.' 2>/dev/null \
+		cat "${TMP_RESPONSE}" | jq -r '.' 2>/dev/null \
 			>"${RESP_BODY}"
 		if [[ -z "$(cat "${RESP_BODY}" 2>/dev/null)" ]];then
 			# 检查是否有加密并解密获取acw_sc__v2令牌
